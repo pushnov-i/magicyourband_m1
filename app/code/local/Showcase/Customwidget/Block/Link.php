76dc438799b6a2a0ae669bd64abc127f5398988f
starengineer <mrg8406@gmail.com>
2019-08-27 04:58:37 -0400

    add the showcase in cms widget

---
+++ app/code/local/Showcase/Customwidget/Block/Link.php
@@ -0,0 +1,28 @@
+<?php
+class Showcase_Customwidget_Block_Links extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface {
+  protected function _toHtml() {
+    $html = '';
+    $link_options = $this­>getData('link_options');
+      
+    if (empty($link_options)) {
+      return $html;
+    }
+      
+    $arr_options = explode(',', $link_options);
+      
+    if (is_array($arr_options) && count($arr_options)) {
+      foreach ($arr_options as $option) {
+        Switch ($option) {
+          case 'print':
+            $html .= '<div><a href="javascript: window.print();">Print</a></div>';
+          break;
+          case 'email':
+            $html .= '<div><a href="mailto:yourcompanyemail@domain.com&subject=Inquiry">Contact Us</a></div>';
+          break;
+        }
+      }
+    }
+     
+    return $html;
+  }
+}
